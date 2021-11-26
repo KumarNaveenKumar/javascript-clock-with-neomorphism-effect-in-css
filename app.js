680ff781c24bcdc11b43713bdfe0f0f4a439339f
Naveen Kumar <81712862+KumarNaveenKumar@users.noreply.github.com>
2021-11-26 12:50:00 +0530

    Update app.js
    Added comments

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+// Use of setInterval() method to repeat the showTime function after every 1s.
 setInterval(showTime, 1000);
+
 function showTime() {
+  
+//   To create a date object.
 const d = new Date();
+  
 const deg = 6;
+  
+//   To get Hours, Minutes and Second.
 const hour = d.getHours();
 const minute = d.getMinutes();
 const second = d.getSeconds();
+  
+//   To set the calculations of the amount of rotation of the hands of the clock.
 const hRotation = hour*30 + minute/2;
 const mRotation = minute*6;
 const sRotation = second*6;
 
+//   To rotate the hands of the clock.
 hr.style.transform = `rotate(${hRotation}deg)`;
 mn.style.transform = `rotate(${mRotation}deg)`;
 sc.style.transform = `rotate(${sRotation}deg)`;
